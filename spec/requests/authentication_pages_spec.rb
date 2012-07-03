877bcbcf58df00b6327cd286bc59277526c43fce
Ricardo Perez <ricas07@gmail.com>
2012-07-03 11:00:58 -0700

    sign in and out complete

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -16,7 +16,7 @@ describe "Authentication" do
       	describe "after visiting another page" do
   			before { click_link "Home" }
   			it { should_not have_selector('div.alert.alert-error') }
-		end
+		  end
     end
 
     describe "with valid information" do
