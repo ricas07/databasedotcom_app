d24e10ce7e570d3d44fc07c533d26ea931032b3a
Ricardo Perez <ricas07@gmail.com>
2012-07-03 11:49:17 -0700

    trying to setup salesforce

--- config/environment.rb
+++ config/environment.rb
@@ -2,4 +2,11 @@
 require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
+# this is a test 
 SampleApp::Application.initialize!
+
+# Rails::Initializer.run do |config|   
+  #additional or existing configuration
+#  config.gem "asf-soap-adapter", :lib => 'asf-soap-adapter'  
+# end
+
