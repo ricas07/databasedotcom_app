c6ac03e047b50a5d37315d875463ecc398dbf5da
Ricardo Perez <ricas07@gmail.com>
2012-07-26 15:08:52 -0700

    Forwarding to nice pages when trying to access unauthorized pages.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,10 @@
 class UsersController < ApplicationController
   #include Databasedotcom::Rails::Controller
-  before_filter :signed_in_user, only: [:edit, :update]
+  before_filter :signed_in_user, only: [:index, :edit, :update]
   before_filter :correct_user,   only: [:edit, :update]
   
   def index
-    @users = User.all()[0..19]
+    @users = User.all
   end
 
   def new
