76ffc84fc5756087ca06c48152827d50dcac04b5
Ricardo Perez <ricas07@gmail.com>
2012-08-21 18:00:44 -0700

    got micropost web form working.

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,8 +1,9 @@
 class StaticPagesController < ApplicationController
   def home
-  	if signed_in?
-  		redirect_to current_user
-  	end
+  	#if signed_in?
+  		#redirect_to current_user
+  	#end
+    @micropost = current_user.microposts.build if signed_in?
   end
 
   def help
