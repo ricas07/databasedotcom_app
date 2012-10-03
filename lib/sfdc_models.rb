feba814bc6a880814cbd1a666d6202c581292c3b
Ricardo Perez <ricas07@gmail.com>
2012-10-03 10:02:42 -0700

    updates salesforce password & token

--- lib/sfdc_models.rb
+++ lib/sfdc_models.rb
@@ -2,8 +2,8 @@
 module SFDC_Models 
 
     client = Databasedotcom::Client.new :client_id => "3MVG9y6x0357Hledlw0Op2Vcbey_xXD3gqwk.D9ApXFeNioQCOda082RjQegBOrMyJ4CispsJoJyDAFbc6dIR", :client_secret => "5587993940227999074"
-    client.authenticate :username => "ricas071340318457207@gmail.com", :password => "P@ssword1fgBfS3P0N5B97VQf2SfiW2Q8M"
+    client.authenticate :username => "ricas071340318457207@gmail.com", :password => "P@ssword7JLBoA1xCmLAKR3clwhGJoZ1P"
 
 	client.sobject_module = "SFDC_Models"
     client.materialize("User")
-end	
+end
