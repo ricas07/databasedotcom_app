b73c4d5417f8f341920ea734f7445b0e4e09a2bc
Ricardo Perez <ricas07@gmail.com>
2012-10-03 17:23:26 -0700

    preping app

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,6 @@
 class UsersController < ApplicationController
   include Databasedotcom::Rails::Controller
-  before_filter :signed_in_user, only: [:index, :edit, :update, :destroy, :following, :followers]
-  before_filter :correct_user,   only: [:edit, :update]
-  before_filter :admin_user, only: :destroy 
+  before_filter :signed_in_user, only: [:index, :edit, :update, :destroy]
   
   def index
     @users = User.paginate(page: params[:page])
@@ -14,14 +12,13 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
-    @microposts = @user.microposts.paginate(page: params[:page])
   end
 
   def create
     @user = User.new(params[:user])
     if @user.save
       sign_in @user
-      flash[:success] = "Welcome to the Sample App!"
+      flash[:success] = "Welcome to the R66T Salesforce App!"
       redirect_to @user
     else
       render 'new'
@@ -33,26 +30,12 @@ class UsersController < ApplicationController
 
   def update
     if @user.update_attributes(params[:user])
-      flash[:success] = "Profile updated"
+      flash[:success] = "Account updated"
       sign_in @user
-      redirect_to @user
+      redirect_to root_path
     else
       render 'edit'
     end
-  end
-
-  def following
-    @title = "Following"
-    @user = User.find(params[:id])
-    @users = @user.followed_users.paginate(page: params[:page])
-    render 'show_follow'
-  end
-
-  def followers
-    @title = "Followers"
-    @user = User.find(params[:id])
-    @users = @user.followers.paginate(page: params[:page])
-    render 'show_follow'
   end
 
   def destroy
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,7 +2,7 @@ module ApplicationHelper
 
   # Returns the full title on a per-page basis.
   def full_title(page_title)
-    base_title = "Ricardo's App"
+    base_title = "R66T Salesforce"
     if page_title.empty?
       base_title
     else
